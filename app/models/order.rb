cd721aa8a3733d669e1b8c7d28db51869d84c491
tangjiahong <fruitcake0525@gmail.com>
2016-06-14 22:36:56 +0800

    基本收納術

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,11 @@
 class Order < ActiveRecord::Base
+  include Tokenable
   belongs_to :user
 
   has_many :items, class_name: "OrderItem", dependent: :destroy
   has_one  :info,  class_name: "OrderInfo", dependent: :destroy
 
   accepts_nested_attributes_for :info
-
-  before_create :generate_token
 
   include AASM
 
@@ -40,11 +39,7 @@ class Order < ActiveRecord::Base
     end
   end
 
-  def generate_token
-    self.token = SecureRandom.uuid
-  end
-
-    def build_item_cache_from_cart(cart)
+  def build_item_cache_from_cart(cart)
     cart.items.each do |cart_item|
       item = items.build
       item.product_name = cart_item.title
